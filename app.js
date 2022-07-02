4f073ec3fcf79039c51ca78cb88cd629ff7ed096
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2022-07-02 18:20:29 +0400

    Add data

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// Array of objects containing data for the application
+const data = [
+  {
+    fullName: 'Zoowee Blubberworth',
+    age: 24,
+    quote:
+      'I want my children to have all the things I couldn’t afford. Then I want to move in with them.',
+  },
+  {
+    fullName: 'Flufffy Gloomkins',
+    age: 42,
+    quote:
+      'I used to sell furniture for a living. The trouble was, it was my own.',
+  },
+  {
+    fullName: 'Humster Pottyworthy',
+    age: 28,
+    quote:
+      'Truth hurts. Maybe not as much as jumping on a bicycle with a seat missing, but it hurts.',
+  },
+  {
+    fullName: 'Eggpants Snothall',
+    age: 53,
+    quote:
+      'Common sense is like deodorant. The people who need it most never use it.',
+  },
+  {
+    fullName: 'Sniffpants Pimplehill',
+    age: 18,
+    quote: 'A day without sunshine is like, you know, night.',
+  },
+];
